95a327ac74cafab344b54b44b69a682d31ed54ad
Nick Mazey <nicholasmazey@gmail.com>
2021-11-10 12:58:59 +1300

    changed entity to header + cpp file

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,45 +1,8 @@
 #include <set>
-#include <functional>
-class Entity{
-    private:
-        int id;
+#include "entity.h"
+int Entity::entityCount = 0;
 
-        //Movement
-        int x;
-        int y;
-        int z;
-        int coordVector [3];
-        std::set<Entity *> children;
-
-        //Collision
-        int width;
-        int height;
-        int depth;
-        std::set<Entity> ghosts;
-
-        //Physics
-        bool physics;
-        bool solid = true;
-        int gravity;
-        float friction;
-
-        //Rendering
-        int lookAngX = 0;
-        int lookAngY = 0;
-        int fov = 0;
-        int lookVector [2];
-
-
-    public:
-        //How many entites have been initialised
-        static int entityCount;
-
-        //currently unimplemented
-        bool operator==(const Entity &other) const{
-            return this->id == other.getId();
-        }
-
-        Entity(int x, int y, int z, int width, int height, int depth){
+Entity::Entity(int x, int y, int z, int width, int height, int depth){
             this->x = x;
             this->y = y;
             this->z = z;
@@ -47,71 +10,80 @@ class Entity{
             this->height = height;
             this->depth = depth;
             this->id = entityCount;
-            entityCount++; //Basic unique id
-        }
+            entityCount++;
+}
 
+bool Entity::operator==(const Entity &other) const{
+            return this->id == other.getId();
+}
+
+bool Entity::operator<(const Entity &other) const{
+            return this->id < other.id;
+}
+
+        
         //sets the x y and z move vectors
-        void setMove(int x, int y, int z){
+void Entity::setMove(int x, int y, int z){
             this->coordVector[0] = x;
             this->coordVector[1] = y;
             this->coordVector[2] = z;
-        }
+}
 
 
         //Applies movement
-        void doMove(){
+void Entity::doMove(){
             this->x += coordVector[0];
             this->y += coordVector[1];
             this->z += coordVector[2];
-        }
+}
 
         //updates the entity's x,y, and z co-ordinates by x,y, and z
-        void doMove(int x,int y,int z){
+void Entity::doMove(int x,int y,int z){
             this->setMove(x,y,z);
             this->doMove();
-        }
+}
 
         //sets the look vector angles
-        void setLook(int x,int y){
+void Entity::setLook(int x,int y){
             this->lookVector[0] = x;
             this->lookVector[1] = y;
-        }
+}
 
 
         //Applies look
-        void doLook(){
+void Entity::doLook(){
             this->lookAngX += lookVector[0];
             this->lookAngY += lookVector[1];
-        }
+}
 
         //updates the entity's look angles by x and y
-        void doLook(int x,int y){
+void Entity::doLook(int x,int y){
             this->setLook(x,y);
             doLook();
-        }
+}
 
 
-        void setPos(int x, int y, int z){
+void Entity::setPos(int x, int y, int z){
             this->x = x;
             this->y = y;
             this->z = z;
-        }
+}
 
         //sets position of entity relative to other entity + x,y, and z
-        void setPosRelativeTo(Entity other,int x,int y,int z){
+void Entity::setPosRelativeTo(Entity other,int x,int y,int z){
                 this->x = x + other.getX();
                 this->y = y + other.getY();
                 this->z = z + other.getZ();
-        }
+}
 
         //Whether or not another entity is in the ghosts of this entity
-        bool inGhosts(Entity other){
+bool Entity::inGhosts(Entity other){
             return this->ghosts.find(other) != ghosts.end();
-        }
+}
 
 
         //whether or not this entity is colliding with the other (atm uses bounding box)
-        bool isColliding(Entity other){
+bool Entity::isColliding(Entity other){
             if(!this->inGhosts(other)){
 
             //Variables for this
@@ -154,10 +126,10 @@ class Entity{
 
             //Would've already returned otherwise, must not be colliding
             return false;
-        }
+}
 
         //whether or not this entity would collide with the other if it moved by x,y, and z
-        bool wouldCollide(Entity other, int x,int y,int z){
+bool Entity::wouldCollide(Entity other, int x,int y,int z){
             if(!this->inGhosts(other)){
                 Entity created(this->x,this->y,this->z,this->width,this->height,this->depth);
                 created.doMove(x,y,z);
@@ -165,52 +137,4 @@ class Entity{
             }
             //Can't collide
             return false;
-        }
-
-        //Getters
-        int getId() const{
-            return this->id;
-        }
-        int getX() const{
-            return this->x;
-        }
-        int getY() const{
-            return this->y;
-        }
-        int getZ() const{
-            return this->z;
-        }
-        int getWidth() const{
-            return this->width;
-        }
-        int getHeight() const{
-            return this->height;
-        }
-        int getDepth() const{
-            return this->depth;
-        }
-        int getLookAngX() const{
-            return this->lookAngX;
-        }
-        int getLookAngY() const{
-            return this->lookAngY;
-        }
-        bool isSolid() const{
-            return this->solid;
-        }
-
-        //Setters
-        void setSolid(bool solid){
-            this->solid = solid;
-        }
-};
-
-//Hash function, very simple atm but will work for now
-template<>
-struct std::hash<Entity>
-{
-    std::size_t operator()(Entity const& entity) const noexcept
-    {
-        return std::hash<int>{}(entity.getId());
-    }
-};
+}
---
+++ src/entity.h
@@ -0,0 +1,117 @@
+#include <set>
+#ifndef GAME_ENGINE_ENTIY_H
+#define GAME_ENGINE_ENTIY_H
+class Entity{
+    private:
+        int id;
+
+        //Movement
+        int x;
+        int y;
+        int z;
+        int coordVector [3];
+        std::set<Entity *> children;
+
+        //Collision
+        int width;
+        int height;
+        int depth;
+        std::set<Entity> ghosts;
+
+        //Physics
+        bool physics;
+        bool solid;
+        int gravity;
+        float friction;
+
+        //Rendering
+        int lookAngX;
+        int lookAngY;
+        int fov;
+        int lookVector [2];
+
+
+    public:
+        //How many entites have been initialised
+        static int entityCount;
+
+        bool operator==(const Entity &other) const;
+
+        //currently unfinished
+        bool operator<(const Entity &other) const;
+
+        Entity(int x, int y, int z, int width, int height, int depth);
+
+        //sets the x y and z move vectors
+        void setMove(int x, int y, int z);
+
+
+        //Applies movement
+        void doMove();
+
+        //updates the entity's x,y, and z co-ordinates by x,y, and z
+        void doMove(int x,int y,int z);
+
+        //sets the look vector angles
+        void setLook(int x,int y);
+
+
+        //Applies look
+        void doLook();
+
+        //updates the entity's look angles by x and y
+        void doLook(int x,int y);
+
+
+        void setPos(int x, int y, int z);
+
+        //sets position of entity relative to other entity + x,y, and z
+        void setPosRelativeTo(Entity other,int x,int y,int z);
+
+        //Whether or not another entity is in the ghosts of this entity
+        bool inGhosts(Entity other);
+
+
+        //whether or not this entity is colliding with the other (atm uses bounding box)
+        bool isColliding(Entity other);
+
+        //whether or not this entity would collide with the other if it moved by x,y, and z
+        bool wouldCollide(Entity other, int x,int y,int z);
+
+        //Getters
+        int getId() const{
+            return id;
+        }
+        int getX() const{
+            return x;
+        }
+        int getY() const{
+            return y;
+        }
+        int getZ() const{
+            return z;
+        }
+        int getWidth() const{
+            return width;
+        }
+        int getHeight() const{
+            return height;
+        }
+        int getDepth() const{
+            return depth;
+        }
+        int getLookAngX() const{
+            return lookAngX;
+        }
+        int getLookAngY() const{
+            return lookAngY;
+        }
+        bool isSolid() const{
+            return solid;
+        }
+
+        //Setters
+        void setSolid(bool solid);
+};
+
+#endif //GAME_ENGINE_ENTIY_H
