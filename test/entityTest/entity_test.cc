75dfc3127c27cfe748fb1267b8db3b590fb1b002
Nick Mazey <nicholasmazey@gmail.com>
2021-11-10 16:24:15 +1300

    updated test name and added transitive check

--- test/entityTest/entity_test.cc
+++ test/entityTest/entity_test.cc
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
 #include "entity.h"
-TEST(EntityTest,Collides){
+TEST(EntityTest,Collides_1){
     logic::Entity a (0,0,0,100,100,100);
     logic::Entity b (0,0,0,100,100,100);
     EXPECT_TRUE(a.isColliding(b));
+    EXPECT_EQ(a.isColliding(b),b.isColliding(a));
 }
