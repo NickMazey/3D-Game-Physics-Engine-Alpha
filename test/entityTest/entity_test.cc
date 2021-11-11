4c9ef53cca1027a76238b5640c97456426b05b21
Nick Mazey <nicholasmazey@gmail.com>
2021-11-12 11:48:25 +1300

    added some movement tests

--- test/entityTest/entity_test.cc
+++ test/entityTest/entity_test.cc
@@ -19,16 +19,63 @@ std::string printInfo(const logic::Entity toPrint){
     return toReturn;
 }
 
+//ID Tests
 TEST(EntityTest,Unique_Ids_1){
     logic::Entity a (0,0,0,0,0,0);
     logic::Entity b (0,0,0,0,0,0);
     EXPECT_TRUE(a.getId() != b.getId()) << "The IDs aren't unique. \n Entity a:" << printInfo(a) << "\n Entity b: " << printInfo(b);
 }
 
+TEST(EntityTest, Unique_Ids_2){
+    logic::Entity a (0,0,0,0,0,0);
+    EXPECT_TRUE(a.getId() == a.getId()) << "ID changes between calls. \n Entity:" << printInfo(a);
+}
+
+//Movement Tests
+TEST(EntityTest, Movement_Test_X_1){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(1,0,0);
+    EXPECT_TRUE(a.getX() == 1) << "Entity didn't move + in X axis. \n Entity:" << printInfo(a);
+}
+//Movement Tests
+TEST(EntityTest, Movement_Test_X_2){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(-1,0,0);
+    EXPECT_TRUE(a.getX() == -1) << "Entity didn't move - in X axis. \n Entity:" << printInfo(a);
+}
+
+TEST(EntityTest, Movement_Test_Y_1){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(0,1,0);
+    EXPECT_TRUE(a.getY() == 1) << "Entity didn't move + in Y axis. \n Entity:" << printInfo(a);
+}
+
+TEST(EntityTest, Movement_Test_Y_2){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(0,-1,0);
+    EXPECT_TRUE(a.getY() == -1) << "Entity didn't move - in Y axis. \n Entity:" << printInfo(a);
+}
+
+TEST(EntityTest, Movement_Test_Z_1){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(0,0,1);
+    EXPECT_TRUE(a.getZ() == 1) << "Entity didn't move + in Z axis. \n Entity:" << printInfo(a);
+}
+
+TEST(EntityTest, Movement_Test_Z_2){
+    logic::Entity a (0,0,0,100,100,100);
+    a.doMove(0,0,-1);
+    EXPECT_TRUE(a.getZ() == -1) << "Entity didn't move - in Z axis. \n Entity:" << printInfo(a);
+}
+
+
+//Self Collision Tests
 TEST(EntityTest,Collides_Self_1){
     logic::Entity a (0,0,0,100,100,100);
     EXPECT_TRUE(a.isColliding(a)) << "The Entity doesn't collide with itself. \n Entity: " << printInfo(a);
 }
+
+//Other Entity Collision Tests
 TEST(EntityTest,Collides_Other_1){
     logic::Entity a (0,0,0,100,100,100);
     logic::Entity b (0,0,0,100,100,100);
