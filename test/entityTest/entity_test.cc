060786b039cb8dcfb312414f86f0967d98a60d30
Nick Mazey <nicholasmazey@gmail.com>
2021-11-12 10:17:52 +1300

    changed test to have more useful output

--- test/entityTest/entity_test.cc
+++ test/entityTest/entity_test.cc
@@ -1,8 +1,27 @@
 #include "gtest/gtest.h"
 #include "entity.h"
+
+std::string printInfo(const logic::Entity toPrint){
+    std::string toReturn = "ID: ";
+    toReturn.append("" + toPrint.getId());
+    toReturn.append(" X: ");
+    toReturn.append("" + toPrint.getX());
+    toReturn.append(" Y: ");
+    toReturn.append("" + toPrint.getY());
+    toReturn.append(" Z: ");
+    toReturn.append("" + toPrint.getZ());
+    toReturn.append(" Width: ");
+    toReturn.append("" + toPrint.getWidth());
+    toReturn.append(" Height: ");
+    toReturn.append("" + toPrint.getHeight());
+    toReturn.append(" Depth: ");
+    toReturn.append("" + toPrint.getDepth());
+    return toReturn;
+}
+
 TEST(EntityTest,Collides_1){
     logic::Entity a (0,0,0,100,100,100);
     logic::Entity b (0,0,0,100,100,100);
-    EXPECT_TRUE(!a.isColliding(b)) << "Testing output, a id: " << a.getId();
-    EXPECT_EQ(a.isColliding(b),b.isColliding(a));
+    EXPECT_TRUE(!a.isColliding(b)) << "Entities aren't colliding. \n Entity a: " << printInfo(a) << "\n Entity b: " << printInfo(b);
+    EXPECT_EQ(a.isColliding(b),b.isColliding(a)) << "Collisions aren't transitive";
 }
