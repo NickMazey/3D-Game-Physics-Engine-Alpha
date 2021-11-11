9704d0c3f956fc0e8126132d64b35646b458e419
Nick Mazey <nicholasmazey@gmail.com>
2021-11-12 10:35:06 +1300

    Added tests for self collision and id uniqueness

--- test/entityTest/entity_test.cc
+++ test/entityTest/entity_test.cc
@@ -19,9 +19,19 @@ std::string printInfo(const logic::Entity toPrint){
     return toReturn;
 }
 
-TEST(EntityTest,Collides_1){
+TEST(EntityTest,Unique_Ids_1){
+    logic::Entity a (0,0,0,0,0,0);
+    logic::Entity b (0,0,0,0,0,0);
+    EXPECT_TRUE(a.getId() != b.getId()) << "The IDs aren't unique. \n Entity a:" << printInfo(a) << "\n Entity b: " << printInfo(b);
+}
+
+TEST(EntityTest,Collides_Self_1){
+    logic::Entity a (0,0,0,100,100,100);
+    EXPECT_TRUE(a.isColliding(a)) << "The Entity doesn't collide with itself. \n Entity: " << printInfo(a);
+}
+TEST(EntityTest,Collides_Other_1){
     logic::Entity a (0,0,0,100,100,100);
     logic::Entity b (0,0,0,100,100,100);
-    EXPECT_TRUE(!a.isColliding(b)) << "Entities aren't colliding. \n Entity a: " << printInfo(a) << "\n Entity b: " << printInfo(b);
+    EXPECT_TRUE(a.isColliding(b)) << "Entities aren't colliding. \n Entity a: " << printInfo(a) << "\n Entity b: " << printInfo(b);
     EXPECT_EQ(a.isColliding(b),b.isColliding(a)) << "Collisions aren't transitive";
 }
