92b779e9dbef6bb093bc1b988855b2d73f40c30c
Nick Mazey <nicholasmazey@gmail.com>
2021-11-18 16:50:49 +1300

    Changed PI to 10dp

--- src/logic/util.cc
+++ src/logic/util.cc
@@ -1,11 +1,11 @@
 #include "util.h"
 namespace logic{
-    #define PI 3.14159265
+    #define PI 3.1415926535
     double degreesToRadians(double degrees){
     return (degrees * PI) / 180.0;
 }
     int radiansToDegrees(double radians){
-        return (180 * radians) / PI;
+        return (180.0 * radians) / PI;
     }
     #undef PI
 }
