7c0151393d287136bca83cda264d5c2cc1646bdc
Nick Mazey <nicholasmazey@gmail.com>
2022-01-13 20:06:00 +1300

    added initialised projectile launcher to projectilelaunchertest

--- test/projectileLauncherTest/projectilelauncher_test.cc
+++ test/projectileLauncherTest/projectilelauncher_test.cc
@@ -2,5 +2,6 @@
 #include "projectilelauncher.h"
 
 TEST(ProjectileLauncherTest, EmptyTest){
+    logic::ProjectileLauncher * proj = new logic::ProjectileLauncher(0,0,0,0,0,0,0,0,0);
     EXPECT_TRUE(true);
 }
