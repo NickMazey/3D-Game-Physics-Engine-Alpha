983dbe6e412b8133d606c018cf13ddf267a041f0
Nick Mazey <nicholasmazey@gmail.com>
2022-01-13 20:03:27 +1300

    added constructor definition for projectile launcher

--- src/logic/projectilelauncher.cc
+++ src/logic/projectilelauncher.cc
@@ -1,4 +1,7 @@
 #include "projectilelauncher.h"
 namespace logic{
-    
+    ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage) : Entity(x,y,z,width,height,depth)
+    {
+
+    }
 }
