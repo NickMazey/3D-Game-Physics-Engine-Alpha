0e8fd207942da93f968df870e7480c3b2e41cb62
Nick Mazey <nicholasmazey@gmail.com>
2022-01-14 16:07:53 +1300

    added initialiser for non-hitscan projectile launcher

--- src/logic/projectilelauncher.cc
+++ src/logic/projectilelauncher.cc
@@ -4,4 +4,9 @@ namespace logic{
     {
 
     }
+    
+    ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage, Entity projectile) : ProjectileLauncher(x,y,z,width,height,depth,ammo,magazineSize,damage)
+    {
+
+    }
 }
