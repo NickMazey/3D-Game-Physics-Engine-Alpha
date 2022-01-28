316392dc0b4f402ddbdbc5bee65b46694111b17c
Nick Mazey <nicholasmazey@gmail.com>
2022-01-29 10:53:43 +1300

    added code so init tests pass

--- src/logic/projectilelauncher.cc
+++ src/logic/projectilelauncher.cc
@@ -2,13 +2,39 @@
 namespace logic{
     ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage) : Entity(x,y,z,width,height,depth)
     {
+        setAmmo(ammo);
+        setMagazineSize(magazineSize);
+        setDamage(damage);
+        setHitscan(true);
+        shootOffX, shootOffY, shootOffZ = 0;
+        this->projectile = *this;
+        this->activeProjectile = this;
+        this->lastHit = this;
+        reload();
     }
     
     ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage, Entity projectile) : ProjectileLauncher(x,y,z,width,height,depth,ammo,magazineSize,damage)
     {
+        setHitscan(false);
+        velocity[0] = projectile.getX();
+        velocity[1] = projectile.getY();
+        velocity[2] = projectile.getZ();
+        projectile.setPos(0,0,0);
+        this->projectile = projectile;
     }
 
     void ProjectileLauncher::reload(){
+        if(loadedAmmo < magazineSize){
+        if(ammo > (magazineSize - loadedAmmo)){
+            ammo -= (magazineSize - loadedAmmo);
+            loadedAmmo = magazineSize;
+        } else if(ammo > 0){
+            loadedAmmo += ammo;
+            ammo = 0;
+        } else{
+            loadedAmmo = magazineSize;
+        }
+        }
     }
 
     bool ProjectileLauncher::fire(std::set<Entity*> entities){
@@ -17,7 +43,6 @@ namespace logic{
     }
 
     bool ProjectileLauncher::hasHit(){
-        //Unimplemented
         return false;
     }
 
@@ -30,26 +55,46 @@ namespace logic{
     }
 
     void ProjectileLauncher::setAmmo(const int toSet){
+        if(toSet >= -1){
+            ammo = toSet;
+        } else{
+            ammo = -1;
+        }
     }
 
     void ProjectileLauncher::setMagazineSize(const int toSet){
+        if(toSet >= 0){
+            magazineSize = toSet;
+        } else{
+            magazineSize = 0;
+        }
     }
 
     void ProjectileLauncher::setDamage(const int toSet){
+        if(toSet >= 0){
+            damage = toSet;
+        } else{
+            damage = 0;
+        }
     }
 
     void ProjectileLauncher::setShootOffX(const int toSet){
+        this->shootOffX = toSet;
     }
 
     void ProjectileLauncher::setShootOffY(const int toSet){
+        this->shootOffY = toSet;
     }
 
     void ProjectileLauncher::setShootOffZ(const int toSet){
+        this->shootOffZ = toSet;
     }
 
     void ProjectileLauncher::setHitscan(const bool toSet){
+        this->hitScan = toSet;
     }
 
     void ProjectileLauncher::setProjectile(const Entity toSet){
+        this->projectile = toSet;
     }
 }
