d75dd129231b58c821adaadad4cdc399395ab4cf
Nick Mazey <nicholasmazey@gmail.com>
2022-01-28 15:03:29 +1300

    added findcollisions method for projectilelauncher skeleton

--- src/logic/projectilelauncher.cc
+++ src/logic/projectilelauncher.cc
@@ -2,11 +2,54 @@
 namespace logic{
     ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage) : Entity(x,y,z,width,height,depth)
     {
-
     }
     
     ProjectileLauncher::ProjectileLauncher(int x, int y, int z, int width, int height, int depth, int ammo, int magazineSize, int damage, Entity projectile) : ProjectileLauncher(x,y,z,width,height,depth,ammo,magazineSize,damage)
     {
+    }
 
+    void ProjectileLauncher::reload(){
+    }
+
+    bool ProjectileLauncher::fire(std::set<Entity*> entities){
+        //Unimplemented
+        return false;
+    }
+
+    bool ProjectileLauncher::hasHit(){
+        //Unimplemented
+        return false;
+    }
+
+    Entity* ProjectileLauncher::findFirstCollision(std::set<Entity*> entities){
+        return this;
+    }
+
+    std::vector<Entity*> ProjectileLauncher::findCollisions(std::set<Entity*> entities){
+        return std::vector<Entity*>();
+    }
+
+    void ProjectileLauncher::setAmmo(const int toSet){
+    }
+
+    void ProjectileLauncher::setMagazineSize(const int toSet){
+    }
+
+    void ProjectileLauncher::setDamage(const int toSet){
+    }
+
+    void ProjectileLauncher::setShootOffX(const int toSet){
+    }
+
+    void ProjectileLauncher::setShootOffY(const int toSet){
+    }
+
+    void ProjectileLauncher::setShootOffZ(const int toSet){
+    }
+
+    void ProjectileLauncher::setHitscan(const bool toSet){
+    }
+
+    void ProjectileLauncher::setProjectile(const Entity toSet){
     }
 }
