19d1921b60d1439dcc9eb4df1e2ef5aab6b7c835
Nick Mazey <nicholasmazey@gmail.com>
2022-02-08 09:33:22 +1300

    improved includes list and formatting

--- src/logic/util.cc
+++ src/logic/util.cc
@@ -1,61 +1,62 @@
-#include "util.h"
 #include <math.h>
-namespace logic{
-    #define PI 3.1415926535f
-    float degreesToRadians(const int degrees){
-    return (degrees * PI) / 180.0f;
-}
-    int radiansToDegrees(const float radians){
-        return (180.0f * radians) / PI;
+
+#include "util.h"
+
+namespace logic
+{
+#define PITENDP 3.1415926535f
+    float degreesToRadians(const int degrees)
+    {
+        return (degrees * PITENDP) / 180.0f;
+    }
+    int radiansToDegrees(const float radians)
+    {
+        return (180.0f * radians) / PITENDP;
     }
 
-    const int simplifyAngle(const int degrees){
-        if(-180 <= degrees && degrees <= 180){
+    const int simplifyAngle(const int degrees)
+    {
+        if (-180 <= degrees && degrees <= 180)
+        {
             return degrees;
-        } else {
+        }
+        else
+        {
             int newDegrees = degrees % 360;
-            if(newDegrees > 180){
+            if (newDegrees > 180)
+            {
                 newDegrees = 180 - newDegrees;
-            } 
+            }
             return newDegrees;
         }
     }
 
-    const float simplifyRadians(const float radians){
-        if(-1.0f * PI <= radians && radians <= PI){
+    const float simplifyRadians(const float radians)
+    {
+        if (-1.0f * PITENDP <= radians && radians <= PITENDP)
+        {
             return radians;
-        } else{
-            float newRad = fmod(radians,(2.0f * PI));
-            if(newRad > PI){
-                newRad = PI - newRad;
+        }
+        else
+        {
+            float newRad = fmod(radians, (2.0f * PITENDP));
+            if (newRad > PITENDP)
+            {
+                newRad = PITENDP - newRad;
             }
             return newRad;
         }
     }
 
-    const float approxSin(const float radians){
-        //Based off of Bhaskara I's sine approximation formula, with the ability to do negative angles as well
-        /*
-        float x = simplifyRadians(radians);
-        bool isNegative = false;
-        if(x < 0){
-            isNegative = true;
-        }
-        float sine = (16.0f * fabs(x) * (PI - fabs(x))) / (5.0f * pow(PI,2.0f) - 4.0f * fabs(x) * (PI - fabs(x)));
-        if(isNegative){
-            sine = 0.0f - sine;
-        }
-        return sine;
-        */
-       return sinf(radians);
+    const float approxSin(const float radians)
+    {
+        return sinf(radians);
     }
 
-    const float approxCos(const float radians){
-        /*
-        return approxSin(radians + PI/2.0f);
-        */
-       return cosf(radians);
+    const float approxCos(const float radians)
+    {
+        return cosf(radians);
     }
-    #undef PI
+#undef PITENDP
 
 }
