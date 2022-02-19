6668f69a7a9e3d57f7a2564b540194cd27e3db05
NickMazey <nicholasmazey@gmail.com>
2022-02-19 13:40:14 +1300

    created test for map to ensure it has all of the required fields

---
+++ test/mapTest/map_test.cc
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include <string>
+#include <vector>
+#include "entity.h"
+#include "projectilelauncher.h"
+#include "map.h"
+
+TEST(MapTest, Has_Fields){
+    struct logic::Map map;
+    std::string name = map.name;
+    std::vector<const logic::Entity> level = map.level;
+    std::vector<const logic::Entity> players = map.players;
+    std::vector<const logic::ProjectileLauncher> availableWeapons = map.availableWeapons;
+    std::vector<const std::vector<int> > loadouts = map.loadouts; 
+    char divLine = map.divLine;
+    SUCCEED();
+}
