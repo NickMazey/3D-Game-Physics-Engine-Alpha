8aa170d146f7bf15fc6917d4bbdc9f8db2a4ddc2
NickMazey <nicholasmazey@gmail.com>
2022-02-24 10:51:02 +1300

    created init test for mockcontroller, rest of tests will start as skeletons

--- test/mockControllerTest/mockcontroller_test.cc
+++ test/mockControllerTest/mockcontroller_test.cc
@@ -1,8 +1,12 @@
 #include "gtest/gtest.h"
 #include "mockcontroller.h"
+#include "controller.h"
 
 //Initialisation Tests
-TEST(MockControllerTest, Init)
+TEST(MockControllerTest, Initialises_Properly)
 {
-    SUCCEED();
+    logic::MockController mock = logic::MockController();
+    ASSERT_NO_FATAL_FAILURE({
+        std::set<logic::Controller::ScaledAction> actions = mock.getActions();
+    });
 }
