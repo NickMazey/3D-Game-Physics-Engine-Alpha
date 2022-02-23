f5a1d0f49da412afea558832e72ebc6925d07e3c
NickMazey <nicholasmazey@gmail.com>
2022-02-24 10:22:35 +1300

    added empty class definition to mockcontroller.h

--- src/logic/mockcontroller.h
+++ src/logic/mockcontroller.h
@@ -1,7 +1,11 @@
+#include "controller.h"
+
 #ifndef GAME_ENGINE_LOGIC_MOCKCONTROLLER_H
 #define GAME_ENGINE_LOGIC_MOCKCONTROLLER_H
 namespace logic
 {
+    class MockController : public Controller{
 
+    };
 } //namespace logic
 #endif //GAME_ENGINE_LOGIC_MOCKCONTROLLER_H
