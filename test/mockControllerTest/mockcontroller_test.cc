108b629a454ca201b8c3ecdc96b5d1b830c92638
NickMazey <nicholasmazey@gmail.com>
2022-02-24 11:15:56 +1300

    created skeleton tests

--- test/mockControllerTest/mockcontroller_test.cc
+++ test/mockControllerTest/mockcontroller_test.cc
@@ -10,3 +10,30 @@ TEST(MockControllerTest, Initialises_Properly)
         std::set<logic::Controller::ScaledAction> actions = mock.getActions();
     });
 }
+
+
+
+//Adds Action Tests
+TEST(MockControllerTest, Adds_First_Action){
+
+}
+
+TEST(MockControllerTest, Adds_Any_Action){
+
+}
+
+TEST(MockControllerTest, Uses_Zero_Scale){
+
+}
+
+TEST(MockControllerTest, Uses_One_Scale){
+
+}
+
+TEST(MockControllerTest, Adds_Two_Actions){
+
+}
+
+TEST(MockControllerTest, Resets_On_Query){
+    
+}
