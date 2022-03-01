c0615f608eb897f7d154cdcd2b4a2cdc6fba1eae
NickMazey <nicholasmazey@gmail.com>
2022-03-02 11:44:00 +1300

    added field test for the player struct

---
+++ test/player_test.cc
@@ -0,0 +1,28 @@
+//Copyright 2022 Nicholas Mazey. All rights reserved
+
+#include "player.h"
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "entity.h"
+#include "projectilelauncher.h"
+
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+
+TEST(PlayerTest, Has_Fields){
+        struct game_engine::logic::Player player;
+        game_engine::logic::Controller* controller = player.controller;
+        game_engine::logic::Entity* entity = player.entity;
+        std::vector<game_engine::logic::ProjectileLauncher> inventory = player.inventory;
+        game_engine::logic::ProjectileLauncher* launcher = player.active_projectile_launcher;
+        EXPECT_EQ(controller,nullptr) << "Players' controllers aren't initialised to nullptr";
+        EXPECT_EQ(entity,nullptr) << "Players' entities aren't initialised to nullptr";
+        EXPECT_EQ(inventory.size(),0) << "Players' inventories aren't initialised empty";
+        EXPECT_EQ(launcher,nullptr) << "Players' active launchers aren't initialised to nullptr";
+}
+
+#pragma GCC diagnostic pop
