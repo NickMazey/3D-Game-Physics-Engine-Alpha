6e4626bf61ee866a64ed3f06b062f15cfc1ca4f0
NickMazey <nicholasmazey@gmail.com>
2022-03-02 11:57:13 +1300

    removed active index

--- test/player_test.cc
+++ test/player_test.cc
@@ -15,12 +15,10 @@
 
 TEST(PlayerTest, Has_Fields){
         struct game_engine::logic::Player player;
-        int active_index = player.active_index;
         game_engine::logic::Controller* controller = player.controller;
         game_engine::logic::Entity* entity = player.entity;
         std::vector<game_engine::logic::ProjectileLauncher*> inventory = player.inventory;
         game_engine::logic::ProjectileLauncher* launcher = player.active_projectile_launcher;
-        EXPECT_EQ(active_index,0) << "Players' active indexes aren't initialised to 0";
         EXPECT_EQ(controller,nullptr) << "Players' controllers aren't initialised to nullptr";
         EXPECT_EQ(entity,nullptr) << "Players' entities aren't initialised to nullptr";
         EXPECT_EQ(inventory.size(),0) << "Players' inventories aren't initialised empty";
