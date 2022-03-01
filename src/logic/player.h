67c15e48ce3b5f14fd7d99ac38e08eebcc492dfd
NickMazey <nicholasmazey@gmail.com>
2022-03-02 11:12:17 +1300

    created player.h file

---
+++ src/logic/player.h
@@ -0,0 +1,20 @@
+// Copyright 2022 Nicholas Mazey. All rights reserved
+#include <string>
+#include <vector>
+
+#include "entity.h"
+#include "projectilelauncher.h"
+
+#ifndef GAME_ENGINE_LOGIC_PLAYER_H
+#define GAME_ENGINE_LOGIC_PLAYER_H
+namespace game_engine
+{
+    namespace logic
+    {
+        struct Player
+        {
+         
+        };
+    } // namespace logic
+} // namespace game_engine
+#endif // GAME_ENGINE_LOGIC_PLAYER_H
