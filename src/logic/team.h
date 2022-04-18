331773703b7f00376aa8dcdb9ab33723544518d1
NickMazey <nicholasmazey@gmail.com>
2022-04-18 13:22:27 +1200

    created team.h file

---
+++ src/logic/team.h
@@ -0,0 +1,14 @@
+// Copyright 2022 Nicholas Mazey. All rights reserved
+
+#ifndef GAME_ENGINE_LOGIC_TEAM_H
+#define GAME_ENGINE_LOGIC_TEAM_H
+namespace game_engine
+{
+namespace logic
+{
+class Team{
+
+};
+} // namespace logic
+} // namespace game_engine
+#endif // GAME_ENGINE_LOGIC_TEAM_H
