04895107ac57f0461889733414a458e980ab13a6
NickMazey <nicholasmazey@gmail.com>
2022-04-18 13:47:05 +1200

    created skeleton tests

---
+++ test/team_test.cc
@@ -0,0 +1,78 @@
+//Copyright 2022 Nicholas Mazey. All rights reserved
+#include "team.h"
+
+#include <set>
+
+#include "gtest/gtest.h"
+
+#include "player.h"
+
+
+//Initialisation Tests
+TEST(TeamTest, Init_Values_Set_Properly)
+{
+
+}
+
+TEST(TeamTest, Init_Team_Size_Bounded){
+
+}
+
+//Adding Players Tests
+TEST(TeamTest,Adds_Players){
+
+}
+
+TEST(TeamTest,Adding_Players_Adds_To_Ghosts){
+
+}
+
+TEST(TeamTest,Adding_Players_Limited_By_Team_Size){
+
+}
+
+//Removing Players Tests
+TEST(TeamTest,Removes_Players){
+
+}
+
+TEST(TeamTest,Removing_Unknown_Player_Does_Not_Crash){
+
+}
+
+TEST(TeamTest,Removing_Removes_From_Ghosts){
+
+}
+
+TEST(TeamTest,Removing_Releases_Spot){
+
+}
+
+
+//Changing Score Tests
+TEST(TeamTest,Set_Score){
+
+}
+
+TEST(TeamTest,Adds_Score){
+
+}
+
+TEST(TeamTest,Removes_Score){
+
+}
+
+TEST(TeamTest,Set_Score_Bounded_By_Zero){
+
+}
+
+TEST(TeamTest,Add_Score_Bounded_By_Zero){
+
+}
+
+TEST(TeamTest,Remove_Score_Bounded_By_Zero){
+    
+}
+
+
+
