3d49c70305f7ebe5f9dc0fbae17436ea3da26895
Nick Mazey <nicholasmazey@gmail.com>
2022-11-15 15:17:22 +1300

    created world.h

--- src/logic/world.h
+++ src/logic/world.h
@@ -1,4 +1,9 @@
 // Copyright 2022 Nicholas Mazey. All rights reserved
+#include "<set>"
+
+#include "controller.h"
+#include "entity.h"
+
 #ifndef GAME_ENGINE_LOGIC_WORLD_H
 #define GAME_ENGINE_LOGIC_WORLD_H
 namespace game_engine
@@ -7,6 +12,35 @@ namespace logic
 {
 class World
 {
+public:
+    //Reads input from this world's controllers
+    void process_controllers();
+
+    //Ticks the game world
+    void do_tick();
+
+    //Validates the position of all of the world's objects
+    void validate_positions();
+
+    //Adds an Entity to the list of objects
+    void add_object(Entity* object);
+
+    //Removes an entity from the list of objects
+    void remove_object(Entity* object);
+
+    //Adds a controller to the list of controllers
+    void add_controller(Controller* controller);
+
+    //Removes a controller from the list of controllers
+    void remove_controller(Controller* controller);
+
+    //Returns the list of objects in the game world
+    const std::set<const Entity*> get_objects();
+
+private:
+    std::set<Entity*> entities_;
+    std::set<Controller*> controllers_;
+    int gravity_;
 };
 } // namespace logic
 } // namespace game_engine
